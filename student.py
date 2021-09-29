1908a379f0405daddec3ff57c0d0f2561ecf9cb5
Akhith <47911685+Akhith@users.noreply.github.com>
2021-09-29 19:32:00 +0530

    Create student.py

--- student.py
+++ student.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify,request, flash, url_for, redirect, render_template
-from flask_sqlalchemy import SQLAlchemy
-import pymysql
-from flask import make_response
 import json
+from flask import Flask, request, jsonify, Response
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import make_response
+
 
 
 
@@ -11,19 +12,36 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY']='YOURSECRETKEY'
 #app.config['SQLALCHEMY_DATABASE_URI'] = conn
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:root123@localhost/training"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://newroot:newroot123@localhost/training"
 
 db = SQLAlchemy(app)
 
 class Student(db.Model):
+    __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
     marks = db.Column(db.Integer)
     age = db.Column(db.Integer)
 
-@app.route('/')
+# db.drop_all()
+db.create_all()
+
+
+@app.route('/',methods=['GET'])
 def show_all():
-   return make_response(json.dumps(students = Student.query.all()),200 )
+    # for stud in Student:
+    try:
+        if request.method in ['GET'] :
+            result = []
+            user=Student.query.all()
+            for stud in user:
+                result.append({"id":stud.id,"name":stud.name,"marks":stud.marks,"age":stud.age})
+            #print(result.id)
+            return Response(json.dumps({"data":result}), 200)
+        else:
+            return Response(json.dumps({"message": "Data cannot be fetched"}), 400)
+    except Exception as e:
+         return Response(json.dumps({"message":str(e)}), 400)
 
 
 # def get():
@@ -33,5 +51,5 @@ def show_all():
 #                 for i, value in enumerate(row)) for row in cur.fetchall()]
 #     return jsonify({'myCollection' : r})
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug = True)
